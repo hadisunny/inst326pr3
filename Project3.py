e068e7ce61552bb1cc14dc608d116aa0e3c972b0
togbonna <togbonna@terpmail.umd.edu>
2024-11-18 11:56:03 -0500

    added code

--- Project3.py
+++ Project3.py
@@ -1,1 +1,36 @@
 #project 3
+
+##requirements
+
+#sun, mon, tues, wed, thurs, fri, sat
+#shift 1 = 7am - 1pm
+#shift 2 = 1pm - 7pm
+
+#caregivers 
+#name, phone, email, pay, hours
+
+#calender
+#pay calculator
+#pay report with weekly and monthly totals 
+
+import calendar
+from datetime import datetime, timedelta
+
+class Caregiver:
+    def __init__(self, name, phone, email, pay_rate):
+        self.name = name
+        self.phone = phone
+        self.email = email
+        self.pay_rate = pay_rate
+        self.availability = {'AM': 'available', 'PM': 'available'}
+        self.hours = {'AM': 0, 'PM': 0}
+
+    def set_availability(self, shift, status):
+        self.availability[shift] = status
+
+    def log_hours(self, shift, hours):
+        self.hours[shift] += hours
+
+    def calculate_pay(self):
+        total_hours = sum(self.hours.values())
+        return total_hours * self.pay_rate
