e6cdd8e6bf6b7b4abd58cceb93ea6fe6bfb496bb
aubrahme <brahme.asmita@gmail.com>
2024-11-21 09:55:19 -0500

    weekly pay and changed where the objects are

--- Project3.py
+++ Project3.py
@@ -9,18 +9,23 @@ class Caregiver:
         self.pay_rate = pay_rate
         self.hours = hours 
         self.availability = "available" #since this is the default 
+    
+    def weekly_pay(self):
+        pay =  self.hours * self.pay_rate
+        return pay
 
 
-class Schedule(Caregiver):
+caregivers = [ Caregiver("Sara", "510-678-7756","sara@gmail.com", 20,36 ),
+        Caregiver("Brian", "510-568-7906","brian@gmail.com",20, 48 ),
+        Caregiver("Hana", "510-234-6700","hana@gmail.com",20, 36 ),
+        Caregiver("John", "510-128-1670","john@gmail.com",20, 36 ),
+        Caregiver("Sally", "510-999-9956","sally@gmail.com",20, 36 ),
+        Caregiver("Stephen", "510-248-8421","stephen@gmail.com",20, 36 ),
+
+    #family members, unpaid 
+        Caregiver("Sara", "510-678-7756","sara@gmail.com", 0,48 ),
+        Caregiver("Liz", "510-688-7566","liz@gmail.com", 0,48 )]
+
+
+class Schedule:
     #make caregiver objects 
-    caregivers = [
-    Caregiver("Sara", "510-678-7756","sara@gmail.com", 20,36 ),
-    Caregiver("Brian", "510-568-7906","brian@gmail.com",20, 48 ),
-    Caregiver("Hana", "510-234-6700","hana@gmail.com",20, 36 ),
-    Caregiver("John", "510-128-1670","john@gmail.com",20, 36 ),
-    Caregiver("Sally", "510-999-9956","sally@gmail.com",20, 36 ),
-    Caregiver("Stephen", "510-248-8421","stephen@gmail.com",20, 36 ),
-
-#family members, unpaid 
-    Caregiver("Sara", "510-678-7756","sara@gmail.com", 0,48 ),
-    Caregiver("Liz", "510-688-7566","liz@gmail.com", 0,48 )]
